5cffecfc3804f61316dba20d24a04b9c9f3d90d6
1cedrus <aowiej23s@gmail.com>
2025-05-11 23:55:33 +0700

    Add UI utilities

---
+++ app/src/main/java/com/appsnipp/education/ui/utils/MyUtilsApp.java
@@ -0,0 +1,12 @@
+package com.appsnipp.education.ui.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public final class MyUtilsApp {
+    private MyUtilsApp(){}
+    public static void showToast(Context context,String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
