c5c6f0807b75e2a388cffdf3ad80af2326ae1ec0
TuandG <dttung2113@gmail.com>
2025-05-18 16:24:37 +0700

    #35 Xây dựng tính năng và giao diện cho Course Analysis

---
+++ app/src/main/java/com/appsnipp/education/ui/adapter/CourseStatAdapter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2025. rogergcc
+ */
+
+package com.appsnipp.education.ui.adapter;
+
+import android.location.GnssAntennaInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.appsnipp.education.R;
+import com.appsnipp.education.ui.model.Course;
+import com.appsnipp.education.ui.model.CourseStat;
+import com.appsnipp.education.ui.model.UserProgress;
+
+import java.util.List;
+
+public class CourseStatAdapter extends RecyclerView.Adapter<CourseStatAdapter.CourseStatViewHolder> {
+    private List<Course> courses;
+    private List<Integer> progresses;
+    private CourseStatListener listener;
+
+    public CourseStatAdapter(List<Course> courses, List<Integer> progresses, CourseStatListener listener) {
+        this.courses = courses;
+        this.progresses = progresses;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public CourseStatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_stat_item, parent, false);
+        return new CourseStatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseStatViewHolder holder, int position) {
+        Course course = courses.get(position);
+        int progress = progresses.get(position);
+        holder.progressTv.setText(String.format("%d%% completed", progress));
+        holder.titleCourseTv.setText(course.getCourseTitle());
+        holder.courseIv.setImageResource(course.getImageResource());
+        holder.progressBar.setProgress(progress);
+        holder.linearLayout.setOnClickListener(v -> listener.onCourseStatClicked(course.getId()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return courses.size();
+    }
+
+    static class CourseStatViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView courseIv;
+        private final TextView titleCourseTv;
+        private final TextView progressTv;
+        private final ProgressBar progressBar;
+        private final LinearLayout linearLayout;
+        public CourseStatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            linearLayout = itemView.findViewById(R.id.item_course_stat);
+            courseIv = itemView.findViewById(R.id.image_course_iv);
+            titleCourseTv = itemView.findViewById(R.id.title_course_tv);
+            progressBar = itemView.findViewById(R.id.progress_bar_pb);
+            progressTv = itemView.findViewById(R.id.progress_tv);
+        }
+    }
+
+    public interface CourseStatListener {
+        void onCourseStatClicked(String courseId);
+    }
+}
---
+++ app/src/main/java/com/appsnipp/education/ui/model/CourseStat.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025. rogergcc
+ */
+
+package com.appsnipp.education.ui.model;
+
+import java.util.List;
+
+public class CourseStat {
+    public List<Course> completedCourses;
+    public List<Integer> completedProgress;
+    public List<Course> inProgressCourses;
+    public List<Integer> inProgress;
+    public List<Course> notJoinCourses;
+    public List<Integer> notJoinProgress;
+
+    public CourseStat(
+            List<Course> completedCourses,
+            List<Integer> completedProgress,
+            List<Course> inProgressCourses,
+            List<Integer> inProgress,
+            List<Course> notJoinCourses,
+            List<Integer> notJoinProgress
+    ) {
+        this.completedCourses = completedCourses;
+        this.completedProgress = completedProgress;
+        this.inProgressCourses = inProgressCourses;
+        this.inProgress = inProgress;
+        this.notJoinCourses = notJoinCourses;
+        this.notJoinProgress = notJoinProgress;
+    }
+}
