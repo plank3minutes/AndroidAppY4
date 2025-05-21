c1f70b169ed0e5eba2dfef7195e178d802b490a8
Đỗ Mạnh Hùng <135967819+hungdm1203@users.noreply.github.com>
2025-05-21 22:59:54 +0700

    Update search

--- app/src/main/java/com/appsnipp/education/ui/menucourses/CoursesStaggedFragment.java
+++ app/src/main/java/com/appsnipp/education/ui/menucourses/CoursesStaggedFragment.java
@@ -3,6 +3,8 @@ package com.appsnipp.education.ui.menucourses;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
